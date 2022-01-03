bdc134fc256c8e3104a25a27df7df4c3c4993e14
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-03 15:07:04 -0500

    saving work in feature/debug

--- script.js
+++ script.js
@@ -8,70 +8,80 @@ var generatePassword = function() {
  } 
  else if (parseInt(promptLength) < 8) {
    window.alert("Your password is less than 8 characters, please try again")
-   return generatePassword()
+   return 
  }
  else if (parseInt(promptLength) > 128) {
    window.alert("Your password is more than 128 characters, please try again")
-   return generatePassword()
+   return 
  }
  else {
    window.alert("Your input value is invalid, please try again.")
-   return generatePassword();
+   return 
  };
 
  // choose whether to have uppercase letters
 let chooseUppercase = window.confirm("Do you want to have UPPERCASE letters in your password?")
-if (chooseUppercase) {
-  chooseUppercase = true;
-} else {
-  chooseUppercase = false;
-};
+// if (chooseUppercase) {
+//   chooseUppercase = true;
+// } else {
+//   chooseUppercase = false;
+
 
 // choose whether to have lowercase letters
 let chooseLowercase = window.confirm("Do you want to have LOWERCASE letters in your password?")
-if (chooseLowercase) {
-  chooseLowercase = true;
-} else {
-  chooseLowercase = false;
-};
+// if (chooseLowercase) {
+//   chooseLowercase = true;
+// } else {
+//   chooseLowercase = false;
+// };
 
 //choose whether to have numbers 
 let chooseNumbers = window.confirm("Do you want to have NUMBERS in your password?")
-if (chooseNumbers) {
-  chooseNumbers = true;
-} else {
-  chooseNumbers = false;
-};
+// if (chooseNumbers) {
+//   chooseNumbers = true;
+// } else {
+//   chooseNumbers = false;
+// };
 
 //choose whether to have special characters
 let chooseSpecialCharacters = window.confirm("Do you want to have SPECIAL CHARACTERS in your password?")
-if (chooseSpecialCharacters) {
-  chooseSpecialCharacters = true;
-} else {
-  chooseSpecialCharacters = false;
-};
+// if (chooseSpecialCharacters) {
+//   chooseSpecialCharacters = true;
+// } else {
+//   chooseSpecialCharacters = false;
+// };
 
 //pull characters from charset
 
-let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+{}:<>?-=[];',./"
+let charset = ""
+let  newPassword = ""
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+{}:<>?-=[];',./"
 if (chooseUppercase) {
-  charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  charset = charset + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  newPassword += uppercase.charAt(Math.floor(Math.random() * uppercase.length));
 }
 if (chooseLowercase) {
-  charset = "abcdefghijklmnopqrstuvwxyz"
+  charset =  charset + "abcdefghijklmnopqrstuvwxyz"
+  let lowercase = "abcdefghijklmnopqrstuvwxyz"
+  newPassword += lowercase.charAt(Math.floor(Math.random() * lowercase.length))
 }
 if (chooseNumbers) {
-  charset = "1234567890"
+  charset =  charset + "1234567890"
+  let numbers = "1234567890"
+  newPassword += numbers.charAt(Math.floor(Math.random() * numbers.length))
 }
 if (chooseSpecialCharacters) {
-  charset = "!@#$%^&*()_+{}:<>?-=[];',./</>"
+  charset = charset + "!@#$%^&*()_+{}:<>?-=[];',./</>"
+  let characters = "!@#$%^&*()_+{}:<>?-=[];',./</>"
+  newPassword += characters.charAt(Math.floor(Math.random() * characters.length))
 };
 
-// create password array
+var newPromptLength = promptLength - newPassword.length
 
-let  newPassword = "";
+// create password array... let i = newPassword + ?
 
-for (let i=0; i < promptLength; i++)  {
+for (let i = 0; i < newPromptLength; i++)  {
   newPassword += charset.charAt(Math.floor(Math.random() * charset.length));
 };
 return newPassword;
